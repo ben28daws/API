976f78cd9e7e9b7a6a81c2e93cc41a04ff8d3816
Pankit Nandwani <107253839+ben28daws@users.noreply.github.com>
2023-12-12 17:56:57 +0530

    Add files via upload

---
+++ accountApiData.js
@@ -0,0 +1,32 @@
+import { LightningElement, wire } from 'lwc';
+import fetchData from '@salesforce/apex/ApiCallout.fetchdata';
+
+export default class ApiCalloutComponent extends LightningElement {
+    apiData;
+    error;
+    isDataVisible = false;
+    apiUrl = ''; 
+
+    @wire(fetchData, { apiEndpoint: '$apiUrl' })
+    wiredApiData({ error, data }) {
+        if (data) {
+            this.apiData = JSON.stringify(JSON.parse(data), null, 2);
+            this.error = undefined;
+        } else if (error) {
+            this.apiData = undefined;
+            this.error = error.body.message;
+        }
+    }
+
+    showData() {
+        this.isDataVisible = true;
+    }
+
+    hideData() {
+        this.isDataVisible = false;
+    }
+
+    handleApiUrlChange(event) {
+        this.apiUrl = event.target.value;
+    }
+}
